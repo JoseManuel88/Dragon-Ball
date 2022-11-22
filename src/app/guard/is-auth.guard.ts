138027e1da2e3ae63d4a8e8d1f1ebfd5fd58c636
JoseManuel88 <113127826+JoseManuel88@users.noreply.github.com>
2022-11-23 00:34:41 +0100

    commit reform

--- src/app/guard/is-auth.guard.ts
+++ src/app/guard/is-auth.guard.ts
@@ -14,7 +14,7 @@ export class IsAuthGuard implements CanActivate {
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
 
-      console.log('fdsa');
+      
       this.communitorService.getIsAuth().subscribe(res => {
         if (!res) {
           this.router.navigateByUrl('login');
